b4fa6d6fc4f1ffde77e558723958eb9299cc11b3
Felipe Sanchez <devsanxz@gmail.com>
2025-11-25 18:59:25 -0300

    Changes to be committed: 	renamed:    note/.config/nvim/LICENSE.md -> common/.config/nvim/LICENSE.md 	renamed:    note/.config/nvim/README.md -> common/.config/nvim/README.md 	renamed:    note/.config/nvim/doc/kickstart.txt -> common/.config/nvim/doc/kickstart.txt 	renamed:    note/.config/nvim/doc/tags -> common/.config/nvim/doc/tags 	renamed:    note/.config/nvim/init.lua -> common/.config/nvim/init.lua 	renamed:    note/.config/nvim/kickstart.nvim/.github/ISSUE_TEMPLATE/bug_report.md -> common/.config/nvim/kickstart.nvim/.github/ISSUE_TEMPLATE/bug_report.md 	renamed:    note/.config/nvim/kickstart.nvim/.github/pull_request_template.md -> common/.config/nvim/kickstart.nvim/.github/pull_request_template.md 	renamed:    note/.config/nvim/kickstart.nvim/.github/workflows/stylua.yml -> common/.config/nvim/kickstart.nvim/.github/workflows/stylua.yml 	renamed:    note/.config/nvim/kickstart.nvim/.gitignore -> common/.config/nvim/kickstart.nvim/.gitignore 	renamed:    note/.config/nvim/kickstart.nvim/.stylua.toml -> common/.config/nvim/kickstart.nvim/.stylua.toml 	renamed:    note/.config/nvim/lazy-lock.json -> common/.config/nvim/lazy-lock.json 	renamed:    note/.config/nvim/lazy-lock.json.bak -> common/.config/nvim/lazy-lock.json.bak 	renamed:    note/.config/nvim/lua/custom/plugins/init.lua -> common/.config/nvim/lua/custom/plugins/init.lua 	renamed:    note/.config/nvim/lua/kickstart/health.lua -> common/.config/nvim/lua/kickstart/health.lua 	renamed:    note/.config/nvim/lua/kickstart/plugins/autopairs.lua -> common/.config/nvim/lua/kickstart/plugins/autopairs.lua 	renamed:    note/.config/nvim/lua/kickstart/plugins/debug.lua -> common/.config/nvim/lua/kickstart/plugins/debug.lua 	renamed:    note/.config/nvim/lua/kickstart/plugins/gitsigns.lua -> common/.config/nvim/lua/kickstart/plugins/gitsigns.lua 	renamed:    note/.config/nvim/lua/kickstart/plugins/indent_line.lua -> common/.config/nvim/lua/kickstart/plugins/indent_line.lua 	renamed:    note/.config/nvim/lua/kickstart/plugins/lint.lua -> common/.config/nvim/lua/kickstart/plugins/lint.lua 	renamed:    note/.config/nvim/lua/kickstart/plugins/neo-tree.lua -> common/.config/nvim/lua/kickstart/plugins/neo-tree.lua

--- common/.config/nvim/init.lua
+++ common/.config/nvim/init.lua
@@ -1039,7 +1039,7 @@ require("lazy").setup({
 		},
 	},
 })
-vim.cmd.colorscheme("sanxz2")
+vim.cmd.colorscheme("sanxz")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
